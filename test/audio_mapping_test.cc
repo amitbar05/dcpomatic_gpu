44cb812612b1db19350c0b42b4e05a193747f55f
Carl Hetherington <cth@carlh.net>
2026-05-17 00:05:51 +0200

    White space: audio_mapping_test.cc

--- test/audio_mapping_test.cc
+++ test/audio_mapping_test.cc
@@ -36,71 +36,71 @@ using std::string;
 using boost::optional;
 
 
-BOOST_AUTO_TEST_CASE (audio_mapping_test)
+BOOST_AUTO_TEST_CASE(audio_mapping_test)
 {
 	AudioMapping none;
-	BOOST_CHECK_EQUAL (none.input_channels(), 0);
+	BOOST_CHECK_EQUAL(none.input_channels(), 0);
 
-	AudioMapping four (4, MAX_DCP_AUDIO_CHANNELS);
-	BOOST_CHECK_EQUAL (four.input_channels(), 4);
+	AudioMapping four(4, MAX_DCP_AUDIO_CHANNELS);
+	BOOST_CHECK_EQUAL(four.input_channels(), 4);
 
-	four.set (0, 1, 1);
+	four.set(0, 1, 1);
 
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < MAX_DCP_AUDIO_CHANNELS; ++j) {
-			BOOST_CHECK_EQUAL (four.get(i, j), (i == 0 && j == 1) ? 1 : 0);
+			BOOST_CHECK_EQUAL(four.get(i, j), (i == 0 && j == 1) ? 1 : 0);
 		}
 	}
 
-	auto mapped = four.mapped_output_channels ();
-	BOOST_CHECK_EQUAL (mapped.size(), 1U);
-	BOOST_CHECK_EQUAL (mapped.front(), 1);
+	auto mapped = four.mapped_output_channels();
+	BOOST_CHECK_EQUAL(mapped.size(), 1U);
+	BOOST_CHECK_EQUAL(mapped.front(), 1);
 
-	four.make_zero ();
+	four.make_zero();
 
 	for (int i = 0; i < 4; ++i) {
 		for (int j = 0; j < MAX_DCP_AUDIO_CHANNELS; ++j) {
-			BOOST_CHECK_EQUAL (four.get (i, j), 0);
+			BOOST_CHECK_EQUAL(four.get(i, j), 0);
 		}
 	}
 }
 
 
 static void
-guess_check (boost::filesystem::path filename, int output_channel)
+guess_check(boost::filesystem::path filename, int output_channel)
 {
-	AudioMapping m (1, 8);
-	m.make_default (0, filename);
+	AudioMapping m(1, 8);
+	m.make_default(0, filename);
 	for (int i = 0; i < 8; ++i) {
 		BOOST_TEST_INFO(fmt::format("{} channel {}", filename.string(), i));
-		BOOST_CHECK_CLOSE (m.get(0, i), i == output_channel ? 1 : 0, 0.01);
+		BOOST_CHECK_CLOSE(m.get(0, i), i == output_channel ? 1 : 0, 0.01);
 	}
 }
 
 
-BOOST_AUTO_TEST_CASE (audio_mapping_guess_test)
+BOOST_AUTO_TEST_CASE(audio_mapping_guess_test)
 {
-	guess_check ("stuff_L_nonsense.wav", 0);
-	guess_check ("stuff_nonsense.wav", 2);
-	guess_check ("fred_R.wav", 1);
-	guess_check ("jim_C_sheila.aiff", 2);
-	guess_check ("things_Lfe_and.wav", 3);
-	guess_check ("weeee_Ls.aiff", 4);
-	guess_check ("try_Rs-it.wav", 5);
+	guess_check("stuff_L_nonsense.wav", 0);
+	guess_check("stuff_nonsense.wav", 2);
+	guess_check("fred_R.wav", 1);
+	guess_check("jim_C_sheila.aiff", 2);
+	guess_check("things_Lfe_and.wav", 3);
+	guess_check("weeee_Ls.aiff", 4);
+	guess_check("try_Rs-it.wav", 5);
 
 	/* PT-style */
-	guess_check ("things_LFE.wav", 3);
-	guess_check ("ptish_Lsr_abc.wav", 6);
-	guess_check ("ptish_Rsr_abc.wav", 7);
-	guess_check ("more_Lss_s.wav", 4);
-	guess_check ("other_Rss.aiff", 5);
+	guess_check("things_LFE.wav", 3);
+	guess_check("ptish_Lsr_abc.wav", 6);
+	guess_check("ptish_Rsr_abc.wav", 7);
+	guess_check("more_Lss_s.wav", 4);
+	guess_check("other_Rss.aiff", 5);
 
 	/* Only the filename should be taken into account */
-	guess_check ("-Lfe-/foo_L.wav", 0);
+	guess_check("-Lfe-/foo_L.wav", 0);
 
 	/* Dolby-style */
-	guess_check ("jake-Lrs-good.wav", 6);
-	guess_check ("elwood-Rrs-good.wav", 7);
+	guess_check("jake-Lrs-good.wav", 6);
+	guess_check("elwood-Rrs-good.wav", 7);
 }
 
 
